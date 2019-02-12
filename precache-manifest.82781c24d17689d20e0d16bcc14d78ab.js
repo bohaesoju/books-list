c219df0062767d1f4d28ee5fc656a977638ff250
bohaesoju <bohaesoju@gmail.com>
2019-02-12 13:40:01 +0900

    Updates

---
+++ precache-manifest.82781c24d17689d20e0d16bcc14d78ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd97fed0935b06c399ff",
+    "url": "/books-list/static/js/runtime~main.fd97fed0.js"
+  },
+  {
+    "revision": "60648343b5c8be79f37f",
+    "url": "/books-list/static/js/main.60648343.chunk.js"
+  },
+  {
+    "revision": "ba59fe894365b859e575",
+    "url": "/books-list/static/js/1.ba59fe89.chunk.js"
+  },
+  {
+    "revision": "60648343b5c8be79f37f",
+    "url": "/books-list/static/css/main.8f75daed.chunk.css"
+  },
+  {
+    "revision": "15b6ed65fdebd5ef8f15f3264c2d3955",
+    "url": "/books-list/index.html"
+  }
+];
